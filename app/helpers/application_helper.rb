b7050f2b172171a1f9a54ce407164e8efd7eac66
Edson Ma <edsonma@gmail.com>
2019-10-03 17:29:23 -0300

    Added User Creation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+private
+
+def logged_in?
+  @current_user ||= User.find(session[:user_id]) if session[:user_id]
 end
+
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  resources :users
+  resources :sessions
+
   # Main page
   root 'home#index'
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 end
