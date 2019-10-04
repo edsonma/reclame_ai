b9d9af68b4ece5108f126f55e3c9f109f15ad180
Edson Ma <edsonma@gmail.com>
2019-10-04 19:04:37 -0300

    Added Complains controller

--- app/controllers/complains_controller.rb
+++ app/controllers/complains_controller.rb
@@ -1,14 +1,9 @@
 class ComplainsController < ApplicationController
-  def new
-    @complain = Complain.new
-    @locale = Locale.new
-  end
-
   def show
     @complain = Complain.find(params[:id])
   end
 
   def index
-    @commplains = Complain.all
+    @complains = Complain.all
   end
 end
