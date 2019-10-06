b1cb32025d4b45db8da6f57596c445d533dd3197
Edson Ma <edsonma@gmail.com>
2019-10-06 04:02:01 -0300

    Added bootstrap to proejct and got better navigation bar and footer

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   root 'home#index'
 
   #get 'orders', to: 'orders#index', as: 'orders'
+  get 'home', to: 'home#index', as: 'home'
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
