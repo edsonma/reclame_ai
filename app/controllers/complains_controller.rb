ca58591f0dc5e12ee8b33e82add913da18bea486
Edson Ma <edsonma@gmail.com>
2019-10-07 19:33:48 -0300

    temporary

--- app/controllers/complains_controller.rb
+++ app/controllers/complains_controller.rb
@@ -1,4 +1,6 @@
+#require 'rest-client '
 class ComplainsController < ApplicationController
+
   def new
     @complain = Complain.new
   end
@@ -6,10 +8,14 @@ class ComplainsController < ApplicationController
   def create
     @complain = Complain.new(allowed_params)
     @complain.maybe_fraud = false;
+
+    # Check if the order supplied by a Complaining person exists on the order table
+    check_existence_of_order
+
     @complain.ip_address = request.remote_ip
 
     if @complain.save
-      redirect_to root_url, notice: 'Agradeço o envio!'
+      redirect_to root_url, notice: 'Agradeço e espero podermos ajudar!'
     else
       redirect_to root_path
     end
@@ -29,4 +35,11 @@ class ComplainsController < ApplicationController
     params.require(:complain).permit!
   end
 
+  def check_existence_of_order
+    order = Order.find_by(shipping_zipcode: @complain.shipping_zipcode)
+    if order == nil
+      puts "heyyyyyyyY"
+      @complain.maybe_fraud=true
+    end
+  end
 end
